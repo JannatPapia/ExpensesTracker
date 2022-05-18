54c8658de7fed652ddf6406d52cb9b3c4859c6aa
Jannatpapia <papiamcode@gmail.com>
2022-05-19 01:45:31 +0600

    Project setUp

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jannatun Nahar Papia  on 19/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
