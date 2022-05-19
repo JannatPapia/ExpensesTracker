d67c75819caf51da478b9e8bbf923a302406f830
Jannatpapia <papiamcode@gmail.com>
2022-05-20 02:58:19 +0600

    Preview Preferred Color Scheme

--- Extention.swift
+++ Extention.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 extension Color {
     static let background = Color("Background")
-    static let icon = Color("Icon")
+    static let icon = Color("icon")
     static let text = Color("Text")
     static let systemBackground = Color(uiColor: .systemBackground)
 }
