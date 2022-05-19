5b26dffc1569a4c082c3d9085e7156538e30450e
Jannatpapia <papiamcode@gmail.com>
2022-05-19 21:45:57 +0600

    Color Extension

---
+++ Extention.swift
@@ -0,0 +1,16 @@
+//
+//  Extention.swift
+//  ExpenseTracker
+//
+//  Created by Jannatun Nahar Papia  on 19/5/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
